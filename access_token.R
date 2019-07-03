87f0461447816d321fa0e24b585abc47334f1fdf
Nissa Ferm <nissa.ferm@gmail.com>
2019-07-03 13:44:42 +0200

    final edit

--- access_token.R
+++ access_token.R
@@ -1,4 +1,4 @@
-Sys.setenv(SPOTIFY_CLIENT_ID = '6d375c1d8d904765852f8a7e07f8758f')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = 'e54289a3745943388ef262e60cd801b6')
+Sys.setenv(SPOTIFY_CLIENT_ID = '')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '')
 
 access_token <- get_spotify_access_token()
